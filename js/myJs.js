bd8509c97ccb6fcb2261c9d21dd3c7f264c1535b
JYR35VVWWJ <JYR35VVWWJ@192.168.1.6>
2024-05-21 21:25:19 +0700

    updated content

--- js/myJs.js
+++ js/myJs.js
@@ -69,7 +69,7 @@ const textConfig = {
   text1: "Meeting you cracked my heart open, now it's forever changed. And because of that, I will carry a part of you with me wherever I go.",
   // text2: "Helu Nga, anh có điều này muốn hỏi, cần Nga trả lời thật lòng",
   text3: "Rachael, takes my energy ༼ つ ◕_◕ ༽つ",
-  text4: "I just miss you today",
+  text4: "Wishing all the best things always come to you - from an aggressive one who always wants to be the best",
   text5: "Be sad",
   text6: "Be happy",
   text7: "I will still be here, no matter what happens",
