e5c4b965b47bb5bd4cf52ef88dfe1633a10afc1e
JYR35VVWWJ <JYR35VVWWJ@AL02357657.local>
2024-10-11 18:04:46 +0700

    update music

--- js/myJs.js
+++ js/myJs.js
@@ -68,7 +68,7 @@ function getTime() {
 const textConfig = {
   text1: "Meeting you cracked my heart open, now it's forever changed. And because of that, I will carry a part of you with me wherever I go 🍀",
   text3: "Rachael, takes my energy ༼ つ ◕_◕ ༽つ",
-  text4: "Ở đây có em Cún thúi",
+  text4: "",
   text5: "Be sad",
   text6: "Be happy",
   text7: "I will still be here, no matter what happens",
