6129e8b9b1aaf7b063fa56cff62adcd7aceee3c5
JYR35VVWWJ <JYR35VVWWJ@AL02357657.local>
2025-01-22 20:02:54 +0700

    Update

--- js/myJs.js
+++ js/myJs.js
@@ -68,7 +68,7 @@ function getTime() {
 const textConfig = {
   text1: "Meeting you cracked my heart open, now it's forever changed. And because of that, I will carry a part of you with me wherever I go 🍀",
   text3: "Rachael, takes my energy ༼ つ ◕_◕ ༽つ",
-  text4: "I hate all the girls out there, they're not you",
+  text4: "",
   text5: "Be sad",
   text6: "Be happy",
   text7: "I will still be here, no matter what happens",
