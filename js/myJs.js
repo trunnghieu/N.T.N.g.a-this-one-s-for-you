e107d906a7a028cba65892365c451d6cd7b054c9
JYR35VVWWJ <JYR35VVWWJ@AL02357657.local>
2025-02-16 22:43:18 +0700

    updated

--- js/myJs.js
+++ js/myJs.js
@@ -75,9 +75,9 @@ function getTime() {
 
 // Main
 const textConfig = {
-  text1: "Day 15 ~ Vẫn còn chút sang chấn Valentine ",
+  text1: "Day 16 ~ Missing you",
   text3: "༼ つ ◕◕ ༽つ🍀🍀🍀",
-  text4: "Chẹp, tui đã chuẩn bị tinh thần cả rồi, mà rốt cuộc trong đầu vẫn tưởng tượng ra hình ảnh chị bên người ta (ᴗ_ᴗ。). Insta nó biết tui thất tình hay sao đó, toàn gợi ý reels về phim có cảnh mấy cặp đôi chia tay. Dù thật lòng muốn thấy chị hạnh phúc, tui vẫn chưa chấp nhận được chuyện trong cuộc sống của tui với chị không có người còn lại. Chị nói tui thời gian sẽ chữa lành được mọi thứ, tui biết chứ, nhưng tui đâu có muốn quên chị đâu. Mỗi ngày cái đầu quỷ của tui cũng tua lại hình ảnh của chị, nụ cười của chị, chắc suy giai đoạn cuối rồi, thời gian không chữa nổi bệnh nan y. Chị làm tui thương nhớ, xong chị để tui ở lại với thời gian vậy mà coi được. Sau này tui sẽ phốt chị cho cả thế giới biết là chị xinh xắn, dịu dàng, giỏi giang nhưng chị đem con bỏ chợ. Mà rồi tui cũng bỏ qua cho chị thôi à, tại chắc tui thương chị quá, đao đớn thiệt. Nhưng nếu để đổi lấy cơ hội được ở cạnh chị lần nữa, tui vẫn còn đao được ~.~",
+  text4: "Dạo này tui ngủ cứ bị thức giấc ấy, khoảng độ 4h sáng, mỗi lần thức giấc tui sẽ lại nghĩ đến chị đầu tiên, chẹp. Lúc tỉnh táo ban ngày tui nghĩ về chuyện hiện tại của chị, lúc mơ màng buổi đêm tui lại nghĩ về kỉ niệm cũ của hai đứa. Chị có đang thật sự hạnh phúc không? Tui hay thắc mắc lắm. Ngày trước tui thấy chị còn hay đăng ảnh trồng cây trồng rau, giờ tui chẳng thấy chị đăng gì nữa, tui cũng lo nhưng lại chẳng thể nhắn cho chị được. Đừng tự làm bản thân mình buồn nhé. Hi vọng mọi thứ sẽ thuận lợi với chị.",
   text5: "Be sad",
   text6: "Be happy",
   text7: "I will still be here, no matter what happens",
