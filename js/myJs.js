9ea83371dbbb1e694dcffb2872338d1c4464b701
JYR35VVWWJ <JYR35VVWWJ@192.168.1.20.non-exists.ptr.local>
2025-07-12 18:37:01 +0700

    updated

--- js/myJs.js
+++ js/myJs.js
@@ -78,7 +78,7 @@ function getTime() {
 const textConfig = {
   text1: "Us back in the old days",
   text3: "180",
-  text4: "",
+  text4: "Chị có nhớ hạt thóc ngốc nghếch không?",
   text5: "Be sad",
   text6: "Be happy",
   text7: "I will still be here, no matter what happens",
